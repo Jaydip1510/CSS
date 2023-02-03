6749fdfd238092c5803c78a913e927a9c41ade4a
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-02-03 10:52:05 +0530

    Add files via upload

---
+++ prj1/ash.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int main(){
+
+ int a=10,b=20;
+ printf("Before swapping a=%d, b=%d",&a,&b);
+ a=a+b;
+ b=a-b;
+ a=a-b;
+ printf("After swapping a=%d,b=%d", &a,&b);
+ return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
